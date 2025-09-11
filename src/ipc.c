950b29b69ac5816569f5f9dca941a6fa63ceca36
matepirola <132600198+matepirola@users.noreply.github.com>
2025-09-11 19:09:23 -0300

    Add analysis results (PVS-Studio, Valgrind)
    Add static analysis report (PVS-Studio) and memory check log (Valgrind).
    
    Co-Authored-By: nmazzitelli <111613730+nmazzitelli@users.noreply.github.com>
    Co-Authored-By: Keoni Lucas Dubovitsky <143182572+keodubo@users.noreply.github.com>

--- src/ipc.c
+++ src/ipc.c
@@ -1,3 +1,7 @@
+// This is a personal academic project.
+// Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
 #define _POSIX_C_SOURCE 200809L
 #include "ipc.h"
 #include <fcntl.h>
--- src/master.c
+++ src/master.c
@@ -1,3 +1,8 @@
+
+// This is a personal academic project.
+// Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
 // master: organiza shm, vista y jugadores
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
